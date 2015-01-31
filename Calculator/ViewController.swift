fb85ff4f28a3deb0a089dce369b0524ecb24f750
xcheng <xcheng@bbtv.com>
2015-01-31 14:41:13 -0800

    basic; dot; sin cos pi

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Xu Cheng on 2015-01-30.
+//  Copyright (c) 2015 drxcheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    var isTyping = false
+    var operandStack = Array<Double>()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if (isTyping) {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            isTyping = true
+        }
+    }
+
+    @IBAction func addDot() {
+        let isInteger = displayValue % 1 == 0
+        
+        if (isInteger) {
+            display.text = display.text! + "."
+        } else if (!isTyping) {
+            display.text = "0."
+            isTyping = true
+        }
+    }
+    
+//    @IBAction func clearDisplay() {
+//        display.text = "0"
+//        isTyping = false
+//    }
+    
+    @IBAction func enter() {
+        isTyping = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue;
+        }
+        set {
+            display.text = "\(newValue)";
+            isTyping = false;
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if isTyping {
+            enter()
+        }
+        switch operation {
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "π": performOperation { M_PI * $0 }
+        case "√": performOperation { sqrt($0) }
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+}
+
