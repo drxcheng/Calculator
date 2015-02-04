bbab70d071896a6ab33a531cc6d7c646fbc6a3e3
xcheng <xcheng@bbtv.com>
2015-02-03 16:24:40 -0800

    MVC

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,99 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Xu Cheng on 2015-02-03.
+//  Copyright (c) 2015 drxcheng. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private enum Op: Printable
+    {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch  self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knowOps = [String: Op]()
+
+    init()
+    {
+        func learnOp(op: Op) {
+            knowOps[op.description] = op
+        }
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.BinaryOperation("−") { $1 - $0 })
+        learnOp(Op.BinaryOperation("×", *))
+        learnOp(Op.BinaryOperation("÷") { $1 / $0 })
+        learnOp(Op.UnaryOperation("√", sqrt))
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op])
+    {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+    
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double?
+    {
+        let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double?
+    {
+        opStack.append(Op.Operand(operand))
+        
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double?
+    {
+        if let operation = knowOps[symbol] {
+            opStack.append(operation)
+        }
+        
+        return evaluate()
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController
     @IBOutlet weak var display: UILabel!
     
     var isTyping = false
-    var operandStack = Array<Double>()
+    
+    var brain = CalculatorBrain()
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -28,8 +29,11 @@ class ViewController: UIViewController
     
     @IBAction func enter() {
         isTyping = false
-        operandStack.append(displayValue)
-        println("operandStack = \(operandStack)")
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
     }
     
     var displayValue: Double {
@@ -38,36 +42,19 @@ class ViewController: UIViewController
         }
         set {
             display.text = "\(newValue)";
-            isTyping = false;
         }
     }
     
     @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
         if isTyping {
             enter()
         }
-        switch operation {
-        case "+": performOperation { $0 + $1 }
-        case "−": performOperation { $1 - $0 }
-        case "×": performOperation { $0 * $1 }
-        case "÷": performOperation { $1 / $0 }
-        case "√": performOperation { sqrt($0) }
-        default: break
-        }
-    }
-    
-    func performOperation(operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func performOperation(operation: Double -> Double) {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            enter()
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
         }
     }
 }
