7aeee9034d13901f3f7cbdca4705474682a8ffc9
xcheng <xcheng@bbtv.com>
2015-02-14 23:07:02 -0800

    extend Assignment 1 using MVC

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,10 +15,26 @@ class ViewController: UIViewController
     @IBOutlet weak var history: UILabel!
     
     var isTyping = false
-    var isOperation = false
-    var operandStack = Array<Double>()
+
+    var brain = CalculatorBrain()
     
-    @IBAction func appendDigit(sender: UIButton) {
+    var displayValue: Double?
+    {
+        get {
+            var number = NSNumberFormatter().numberFromString(display.text!);
+            return number == nil ? nil : number!.doubleValue
+        }
+        set {
+            if newValue == nil {
+                display.text = "0"
+            } else {
+                display.text = "\(newValue!)"
+            }
+        }
+    }
+    
+    @IBAction func appendDigit(sender: UIButton)
+    {
         let digit = sender.currentTitle!
         
         if isTyping && display.text != "0" {
@@ -29,7 +45,8 @@ class ViewController: UIViewController
         }
     }
 
-    @IBAction func addDot() {
+    @IBAction func addDot()
+    {
         let isInteger = displayValue! % 1 == 0
         
         if isInteger {
@@ -40,83 +57,29 @@ class ViewController: UIViewController
         isTyping = true
     }
     
-    @IBAction func enter() {
-        if !isOperation {
-            if (history.text?.isEmpty == true) {
-                history.text = display.text!
-            } else {
-                history.text = history.text! + "," + display.text!
-            }
-        }
+    @IBAction func enter()
+    {
         isTyping = false
-        if displayValue != nil {
-            operandStack.append(displayValue!)
+        if let result = brain.pushOperand(displayValue!) {
+            displayValue = result
+        } else {
+            displayValue = 0
         }
-//        println(operandStack)
+        
+        brain.addHistory(display.text!)
+        history.text = brain.getHistory()
     }
     
-    var displayValue: Double? {
-        get {
-            var number = NSNumberFormatter().numberFromString(display.text!);
-            return number == nil ? nil : number!.doubleValue
-        }
-        set {
-            if newValue == nil {
-                display.text = "0"
-            } else if newValue! % 1 == 0 {
-                var newText = "\(newValue!)"
-                display.text = newText.substringToIndex(advance(newText.startIndex, countElements(newText) - 2))
-            } else {
-                display.text = "\(newValue!)";
-            }
-            isTyping = false;
-        }
-    }
-    
-    @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
-        if isTyping {
-            enter()
-        }
-        isOperation = true
-        switch operation {
-        case "+": performOperation { $0 + $1 }
-        case "−": performOperation { $1 - $0 }
-        case "×": performOperation { $0 * $1 }
-        case "÷": performOperation { $1 / $0 }
-        case "sin": performOperation { sin($0) }
-        case "cos": performOperation { cos($0) }
-        case "π": performOperation { M_PI * $0 }
-        case "√": performOperation { sqrt($0) }
-        default: break
-        }
-        history.text = history.text! + "," + operation + "," + display.text! + "="
-        isOperation = false
-    }
-    
-    func performOperation(operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func performOperation(operation: Double -> Double) {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    @IBAction func clear() {
+    @IBAction func clear()
+    {
         display.text = "0"
         history.text = ""
         isTyping = false
-        isOperation = false
-        operandStack = Array<Double>()
+        brain.clear()
     }
     
-    @IBAction func backspace() {
+    @IBAction func backspace()
+    {
         var displayTextLength = countElements(display.text!)
         
         if displayTextLength > 1 && display.text! != "0" {
@@ -126,8 +89,23 @@ class ViewController: UIViewController
         }
     }
     
-    @IBAction func sign() {
+    @IBAction func sign()
+    {
         displayValue = 0 - displayValue!
+    }
+    
+    @IBAction func operate(sender: UIButton)
+    {
+        if isTyping {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
     }
 }
 
